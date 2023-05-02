a6f8e3ddeecfceeec0a64f296fd11f670dcbbbbb
vinkin56 <126317795+vinkin56@users.noreply.github.com>
2023-05-02 22:11:57 +0500

    Create serverstatus.sh

---
+++ serverstatus.sh
@@ -0,0 +1,8 @@
+status = $(curl -o /dev/null -Iksw "%{http_code}" http://158.160.41.146:9889)
+
+if [ "$status" = "200" ]
+then
+  echo -e "Server status OK"
+else
+  echo 'Status server ERROR' | mail -s 'Warning server not available' -A /var/log/maillog  it.andreys@yandex.ru
+fi
